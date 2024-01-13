ec79f047f54c903cf146574dd0efb1ad6eefa662
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-01-13 17:40:46 +0600

    data and navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div
+        className="relative font-[Manrope] before:fixed before:left-0 before:top-0 before:-z-10 before:h-[435px] before:w-full before:rounded-bl-3xl
+       before:bg-[#EAE6D7] max-md:px-4 lg:text-lg before:lg:rounded-bl-[79px]"
+      >
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
---
+++ src/assets/data.js
@@ -0,0 +1,76 @@
+const booksData = {
+  books: [
+    {
+      bookName: "The Great Gatsby",
+      authorName: "F. Scott Fitzgerald",
+      price: 12.99,
+      rating: 4.5,
+      publishedYear: 1925,
+      bookImage:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/The_Great_Gatsby_Cover_1925_Retouched.jpg/220px-The_Great_Gatsby_Cover_1925_Retouched.jpg",
+      favorite: true,
+    },
+    {
+      bookName: "To Kill a Mockingbird",
+      authorName: "Harper Lee",
+      price: 14.99,
+      rating: 4.8,
+      publishedYear: 1960,
+      bookImage:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg/220px-To_Kill_a_Mockingbird_%28first_edition_cover%29.jpg",
+      favorite: false,
+    },
+    {
+      bookName: "1984",
+      authorName: "George Orwell",
+      price: 11.99,
+      rating: 4.7,
+      publishedYear: 1949,
+      bookImage:
+        "https://upload.wikimedia.org/wikipedia/en/5/51/1984_first_edition_cover.jpg",
+      favorite: true,
+    },
+    {
+      bookName: "The Hobbit",
+      authorName: "J.R.R. Tolkien",
+      price: 10.99,
+      rating: 4.6,
+      publishedYear: 1937,
+      bookImage:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/TheHobbit_FirstEdition.jpg/220px-TheHobbit_FirstEdition.jpg",
+      favorite: false,
+    },
+    {
+      bookName: "Pride and Prejudice",
+      authorName: "Jane Austen",
+      price: 9.99,
+      rating: 4.9,
+      publishedYear: 1813,
+      bookImage:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/PrideAndPrejudiceTitlePage.jpg/220px-PrideAndPrejudiceTitlePage.jpg",
+      favorite: true,
+    },
+    {
+      bookName: "The Catcher in the Rye",
+      authorName: "J.D. Salinger",
+      price: 13.99,
+      rating: 4.4,
+      publishedYear: 1951,
+      bookImage:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/The_Catcher_in_the_Rye_%281951%2C_first_edition_cover%29.jpg/220px-The_Catcher_in_the_Rye_%281951%2C_first_edition_cover%29.jpg",
+      favorite: false,
+    },
+    {
+      bookName:
+        "https://upload.wikimedia.org/wikipedia/en/thumb/6/6b/Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg/220px-Harry_Potter_and_the_Philosopher%27s_Stone_Book_Cover.jpg",
+      authorName: "J.K. Rowling",
+      price: 15.99,
+      rating: 5.0,
+      publishedYear: 1997,
+      bookImage: "harry_potter.jpg",
+      favorite: true,
+    },
+  ],
+};
+
+export default booksData;
