d7f5bc746820c7c8537e95d5dda074dc3858907c
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-01-13 20:08:23 +0600

    favorite button toggled

--- src/components/Books/Books.jsx
+++ src/components/Books/Books.jsx
@@ -3,11 +3,12 @@ import booksData from "../../assets/data";
 
 export default function Books() {
   const books = booksData.books;
+
   return (
     <>
       <div className="container mx-auto grid grid-cols-1 gap-8 max-w-7xl md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {books.map((book) => (
-          <BookItem key={book.id} book={book.book} />
+          <BookItem key={book.id} book={book} />
         ))}
       </div>
     </>
